98ab6329bd4fbba53f07ffb06cecdf6fb06b64d3
Wang Yiping <wangyiping@4paradigm.com>
2019-12-17 19:14:50 +0800

    add pkg ip

---
+++ backend/common.go
@@ -0,0 +1,27 @@
+package backend
+
+import (
+	"net"
+	"sync"
+
+	"github.com/onesafe/simple-flannel/subnet"
+	"golang.org/x/net/context"
+)
+
+type ExternalInterface struct {
+	Iface     *net.Interface
+	IfaceAddr net.IP
+	ExtAddr   net.IP
+}
+
+type Backend interface {
+	RegisterNetwork(ctx context.Context, wg sync.WaitGroup, config *subnet.Config) (Network, error)
+}
+
+type Network interface {
+	Lease() *subnet.Lease
+	MTU() int
+	Run(ctx context.Context)
+}
+
+type BackendCtor func(sm subnet.Manager, ei *ExternalInterface) (Backend, error)
